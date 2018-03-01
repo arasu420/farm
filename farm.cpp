bbdf09861c7d0e577701a3edd8a0e97fef7b7626
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-03-01 14:04:31 +0530

    Create farm.cpp

---
+++ farm.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  double L,B,AREA;
+   cout << "Enter the L & B: ";
+  cin >>L>>B;
+  AREA=L*B;
+  cout<<"AREA OF FARM="<<AREA;
+  return 0;
+}
